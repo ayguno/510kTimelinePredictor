b40fd88e02eb9a51c20b00808f6c6c3c1c67072f
ayguno <20823263+ayguno@users.noreply.github.com>
2018-06-21 18:29:19 -0400

    Continue ui development

--- app.R
+++ app.R
@@ -12,16 +12,27 @@ ui <- material_page(
         material_column(width = 4,
             
             material_card(title = "Tell me a little bit about your product",
-                          depth = 5
+                          depth = 5,
+                material_number_box(input_id = "sub_prior", 
+                                    label = "Number of prior submissions about this device",
+                                    initial_value = 0, min_value = 0, max_value = 1000),
+                material_radio_button(input_id = "sub_type",
+                                      label = "Type of 510k",
+                                      choices = c("Traditional" = "Traditional",
+                                                  "Special" = "Special"))
+                          
             )
         ),
         
+        # Column to present plot
         material_column(width = 4,
                         
                         material_card(title = "Current predictions for similar devices",
                                       depth = 5
                         )
         ),
+        
+        # Column to provide some metrics
         material_column(width = 4,
                         
                         material_card(title = "This process might take...",
