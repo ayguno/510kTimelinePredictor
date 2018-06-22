b0b18f07fed370e84b2319331cb6cf3a2c738ce9
ayguno <20823263+ayguno@users.noreply.github.com>
2018-06-22 10:00:38 -0400

    Continue input building

---
+++ ui.R
@@ -0,0 +1,59 @@
+library(shiny)
+library(shinymaterial)
+
+ui <- material_page(
+    title = " 510k Timeline Predictor",
+    tags$h3("Page Content"),
+    nav_bar_color = "deep-purple darken-3",
+    
+    material_row(
+    
+        # Column to keep inputs
+        material_column(width = 4,
+            
+            material_card(title = "Tell me a little bit about this product",
+                          depth = 5,
+                tags$h6("About the device:"),
+                material_slider(input_id = "dev_prior", 
+                                    label = "Number of prior submissions about this device",
+                                    initial_value = 0, min_value = 0, max_value = 500),
+                
+                material_slider(input_id = "dev_prior", 
+                                label = "Number of prior submissions about this device",
+                                initial_value = 0, min_value = 0, max_value = 500),
+                
+                tags$h6("About the sponsor:"),
+                material_slider(input_id = "sp_prior", 
+                                label = "Number of prior submissions from this sponsor",
+                                initial_value = 0, min_value = 0, max_value = 500, color = "#ef5350"),
+                
+                
+                tags$h6("About the anticipated submission:"),
+                material_radio_button(input_id = "sub_type",
+                                      label = "Type of 510k",
+                                      choices = c("Traditional" = "traditional",
+                                                  "Special" = "special"))
+                          
+            )
+        ),
+        
+        # Column to present plot
+        material_column(width = 4,
+                        
+                        material_card(title = "Current predictions for similar devices",
+                                      depth = 5
+                        )
+        ),
+        
+        # Column to provide some metrics
+        material_column(width = 4,
+                        
+                        material_card(title = "This process might take...",
+                                      depth = 5
+                        )
+        )
+        
+    )
+)
+
+
