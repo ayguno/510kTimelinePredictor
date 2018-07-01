e577cf3b5656092646fbd5398701fae4e3590edd
ayguno <20823263+ayguno@users.noreply.github.com>
2018-07-01 15:43:26 -0400

    global updated for ubuntu version

--- global.R
+++ global.R
@@ -1,4 +1,11 @@
-reticulate::use_virtualenv('510kTimelinePredictor', required = T)
+if(!file.exists('oz5env')){
+untar('oz5env.tar.xz')
+}
+if(!file.exists('functions')){
+untar('functions.tar.xz')
+}
+
+reticulate::use_virtualenv('oz5env', required = T)
 library(reticulate)
 library(shiny)
 library(shinymaterial)
@@ -10,11 +17,17 @@ library(pdftools)
 library(stringr)
 library(tesseract)
 library(qdapRegex)
+library(tm)
+library(SnowballC)
+library(wordcloud)
+library(shinydashboard)
 
 source("./functions/load_vresults.R")
 source("./functions/collect_user_input.R")
 source("./functions/plot_productcode.R")
 source("./functions/plot_review_adv.R")
+source("./functions/prepare_Wordcloud.R")
+source("./functions/prepare_Barplot.R")
 
 vresults <- load_vresults()
 
